cfc31ae8bc4d070b2d7497c478051eb44b0b8ddb
Julian Correa <juliangcorrea@hotmail.com>
2023-02-26 18:47:32 -0300

    API personalidad de citas aleatorias

---
+++ README.md
@@ -0,0 +1,11 @@
+Predicción de personalidad de cita API
+
+Sitio web que utiliza 2 API, una para obtener una cita aleatoria y la segunda para tratar de predecir qué tipo de rasgo de personalidad muestra el autor en esa cita: pensamiento emocional o racional.
+
+Este proyecto utiliza 2 API:
+
+1.- API de Citaciones [Por Martin Svoboda / https://rapidapi.com/martin.svoboda/api/quotes15/]
+"La API gratuita oficial devuelve una cita aleatoria de las citas mejor calificadas de QuotePark.com".
+
+2.- API de Rasgos de Personalidad [Por Symanto / https://rapidapi.com/symanto-symanto-default/api/personality-traits/]
+"Predice el estilo de toma de decisiones y los valores de un individuo a través de un texto escrito".
---
+++ main.js
@@ -0,0 +1,97 @@
+"use strict"
+
+const btn = document.querySelector(".action-btn")
+const text = document.querySelector(".text")
+const load = document.querySelector(".load")
+const textQuote = document.querySelector(".quote")
+let authorName = ""
+let authorQuote = ""
+
+btn.addEventListener("click", getData)
+
+function getData() {
+    textQuote.textContent = ""
+    text.textContent = ""
+    loading()
+    getQuote().then(getPersonality).then(results)
+}
+
+function results(data) {
+    loading()
+    textQuote.textContent = '"' + authorQuote + '"'
+    text.textContent = authorName + " (" + data + ")"
+}
+
+function getPersonality(quote) {
+    if(quote == undefined){
+        return "Número de intentos agotados"
+    }
+    let x = 1
+    let y = "es"
+    const test2 = {"id": (x.toString()), "language": y, "text": quote}
+    const test = [test2]
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            Accept: 'application/json',
+            'X-RapidAPI-Key': '7943fdd383msh2cee669668952d9p1af3dcjsnf3e9cc28eb73',
+            'X-RapidAPI-Host': 'personality-traits.p.rapidapi.com'
+        },
+        body: JSON.stringify(test)
+    }
+    return fetch('https://personality-traits.p.rapidapi.com/personality', options)
+	.then(res => {
+        if (res.status != 200)
+        {
+            return undefined
+        }
+        return res.json()
+    })
+	.then(data => {
+        if(data == undefined){
+            return "Número de intentos agotados"
+        }
+        return data[0].predictions[0].prediction
+    })
+	.catch(error => console.error(error));
+}
+
+function getQuote() {
+    const options2 = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '7943fdd383msh2cee669668952d9p1af3dcjsnf3e9cc28eb73',
+            'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+        }
+    }
+    return fetch('https://quotes15.p.rapidapi.com/quotes/random/?language_code=es', options2)
+	.then(res => {
+        if(res.status != 200){
+            return undefined
+        }
+        return res.json()
+    })
+	.then(data => {
+        if(data == undefined){
+            authorQuote = "Número de intentos agotados"
+            authorName = "Indefinido"
+            return undefined
+        }
+        else{
+            authorQuote = data.content
+            authorName = data.originator.name
+            return data.content
+        }
+    })
+    .catch(error => console.error(error))
+}
+
+function loading() {
+    if(load.classList.contains("hide")){
+        load.classList.remove("hide")
+    }
+    else{
+        load.classList.add("hide")
+    }
+}
